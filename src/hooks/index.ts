f79879a56d1df8993bbdc5248b3b3b2e8db6e99f
Emmybest1 <emmajerry2016@gmail.com>
2021-10-13 22:05:27 +0300

    Update: exported useTitle

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+export * from './use-title/use-title.hook';
 export * from './use-theme/use-theme.hook';
 export * from './use-uniqueids/use-uniqueids.hook';
 export * from './use-ismounted/use-ismounted.hook';
