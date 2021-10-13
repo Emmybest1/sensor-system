839d7a2fa2c91c3ec15c1ad7812566ed855f6688
Emmybest1 <emmajerry2016@gmail.com>
2021-10-13 19:13:40 +0300

    Update: hooked up redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { rootStore } from 'redux/root.store';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={rootStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('rootApp')
 );
