3836a2475a393f45a4c8c9d7034e9ef3320799f2
Emmybest1 <emmajerry2016@gmail.com>
2021-10-13 22:49:31 +0300

    Update: exported file

--- src/components/partials/index.ts
+++ src/components/partials/index.ts
@@ -2,4 +2,6 @@ export * from './logo/logo.component';
 export * from './error/error.component';
 export * from './input/input.component';
 export * from './button/button.component';
+export * from './loaders/ring/ring.component';
+export * from './loaders/skeleton-ui/skeleton-ui.component';
 export * from './modals/notification/notification.component';
