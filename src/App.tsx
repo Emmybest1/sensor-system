1635fb9d3fddbd292560921b6eb1feec29b4dff4
Emmybest1 <emmajerry2016@gmail.com>
2021-10-13 06:54:00 +0300

    Update: updated file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
 
-const App: React.FC = () => <div>Hello React</div>;
+import { Routes } from 'routes';
+import { NormalizedStyles } from 'styles';
+
+const SkipToMainContentNode = styled.a`
+  background-color: #eee;
+  border: solid 1px #333;
+  border-radius: 5px;
+  color: #333;
+  font-size: 0.9rem;
+  padding: 8px;
+  position: absolute;
+  z-index: 10000;
+  top: 5px;
+  left: -1000px;
+
+  &:focus {
+    left: 0;
+  }
+`;
+
+const App: React.FC = () => (
+  <Fragment>
+    <NormalizedStyles />
+    <SkipToMainContentNode href="#main">Skip to main content</SkipToMainContentNode>
+    <Routes />
+  </Fragment>
+);
 
 export default App;
--- src/components/containers/index.ts
+++ src/components/containers/index.ts
@@ -1,1 +1,1 @@
-export {};
+export * from './error-boundary/error-boundary.component';
